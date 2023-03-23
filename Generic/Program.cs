d4a555e53b7b2a7236ed16314fc59215c941f776
unknown <satya-1298>
2023-03-23 18:04:22 +0530

    Initializing

---
+++ Generic/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace Generic_Demo
+{
+    public class Program
+
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Please enter the first value");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the second value");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please enter the third value");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            int val = IntMax.MaximumNumber(num1, num2, num3);
+            Console.WriteLine("{0}is Greater", val);
+        }
+    }
+}
