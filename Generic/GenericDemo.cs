44c073b0591d5d305df9b5055946215aef71189d
unknown <satya-1298>
2023-03-23 21:51:33 +0530

    Using Generic Method

---
+++ Generic/GenericDemo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProblems
+{
+    public class GenericDemo
+    {
+        public  static void FindMax<T>(T firstvalue, T secondvalue, T thirdvalue) where T : IComparable
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && firstvalue.CompareTo(thirdvalue) > 0)
+                Console.WriteLine(firstvalue + " is maximum");
+            if (secondvalue.CompareTo(firstvalue) > 0 && secondvalue.CompareTo(thirdvalue) > 0)
+                Console.WriteLine(secondvalue + " is maximum");
+            if (thirdvalue.CompareTo(secondvalue) > 0 && thirdvalue.CompareTo(firstvalue) > 0)
+                Console.WriteLine(thirdvalue + " is maximum");
+        }
+    }
+}
