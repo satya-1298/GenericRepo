a834e1c7f2874a4d9b9556f8b72316abd2241cf6
unknown <satya-1298>
2023-03-23 22:06:58 +0530

    Initilizing Values

--- Generic/Program.cs
+++ Generic/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Generic;
+using GenericsProblems;
+using System;
 using System.Collections;
 
 namespace Generic_Demo
@@ -8,15 +10,46 @@ namespace Generic_Demo
     {
         public static void Main(string[] args)
         {
-
             Console.WriteLine("Please enter the first value");
             int num1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter the second value");
             int num2 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("Please enter the third value");
             int num3 = Convert.ToInt32(Console.ReadLine());
-            int val = IntMax.MaximumNumber(num1, num2, num3);
-            Console.WriteLine("{0}is Greater", val);
+            int val1 = Demo.Max(num1, num2, num3);
+            Console.WriteLine("{0}is Greater", val1);
+
+
+            Console.WriteLine("Please enter the first string");
+            string str1 = (Console.ReadLine());
+            Console.WriteLine("Please enter the second string");
+            string str2 = (Console.ReadLine());
+            Console.WriteLine("Please enter the third string");
+            string str3 = (Console.ReadLine());
+            string val2 = Demo.Max1(str1, str2, str3);
+            Console.WriteLine("{0}is Greater", val2);
+
+
+            Console.WriteLine("Please enter the first double");
+            double d1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the second double");
+            double d2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the third double");
+            double d3 = Convert.ToDouble(Console.ReadLine());
+            double val3 = Demo.Max2(d1, d2, d3);
+            Console.WriteLine("{0}is Greater", val3);
+            Console.WriteLine("\n");
+            Console.WriteLine("USING GENERIC METHOD");
+            Console.WriteLine("\n");
+            GenericDemo.FindMax(num1, num2, num3);
+            GenericDemo.FindMax(str1, str2, str3);
+            GenericDemo.FindMax(d1, d2, d3);
+            Console.WriteLine("\n");
+            Console.WriteLine("USING GENERIC CLASS");
+            Console.WriteLine("\n");
+            GenericClass<int>.Find(num1, num2, num3);
+            GenericClass<string>.Find(str1, str2, str3);
+            GenericClass<double>.Find(d1, d2, d3);
         }
     }
 }
